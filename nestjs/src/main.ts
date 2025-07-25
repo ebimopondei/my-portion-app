03c3cafafcbeb6829162c1bc04fb70f853e91845
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-07-25 18:31:47 +0100

    add cors

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors( { 
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://my-portion-app.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
